2f9f1a44825e413a579e62c2b29e6cc7684c1d57
Cédric Krier <ced@b2ck.com>
2009-10-12 18:42:19 +0200

    Prevent reloading caldav

--- caldav.py
+++ caldav.py
@@ -1,3 +1,7 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+_TRYTON_RELOAD = False
+
 from DAV.constants import DAV_VERSION_1, DAV_VERSION_2
 
 DAV_VERSION_1['version'] += ',calendar-auto-schedule'

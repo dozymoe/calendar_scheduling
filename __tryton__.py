d94ce7401892765dd96542701afcb36e5997c86a
Bertrand Chenal <bch@b2ck.com>
2009-10-15 17:48:05 +0200

    Updated french translation

--- __tryton__.py
+++ __tryton__.py
@@ -3,11 +3,13 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name' : 'Calendar Scheduling',
+    'name_fr_FR' : 'Programmation calendrier',
     'version' : '0.0.1',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': 'Add Scheduling support on CalDAV',
+    'description_fr_FR': 'Ajoute la gestion de la programmation d\'évènements au CalDAV',
     'depends' : [
         'ir',
         'res',

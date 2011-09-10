c073121d269eba0ec6cee7330838e871749dfc1f
Cédric Krier <ced@b2ck.com>
2011-09-10 21:20:05 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,7 @@
     'name_es_CO' : 'Planificador del calendario',
     'name_es_ES' : 'Planificador del calendario',
     'name_fr_FR' : 'Programmation calendrier',
-    'version' : '1.8.1',
+    'version' : '1.8.2',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
